f3101c5ea165cc18550ff7ab9082b532ff68dcc2
Shubham Pal <115461478+Shubham0126@users.noreply.github.com>
2023-03-17 19:32:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// const main = document.getElementsByClassName("home-heading");
+
+// if(window.innerWidth <= 1200){
+//   console.log("Success");
+//   main.classList.add("center");
+// }
+// else{
+//   console.log("failed");
+// }
+
+const main = document.getElementById('home');
+
+if (window.innerWidth <= 1200) {
+  console.log('Success');
+  main.classList.add('center');
+} else {
+  console.log('failed');
+}
